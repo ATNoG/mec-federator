82e027861e9840f273053ad6cd1e0f4711f50127
Miguel Matos <miguelamatos@ua.pt>
2025-05-26 16:10:46 +0100

    file renaming

--- internal/models/dto/federation.go
+++ internal/models/dto/federation.go
@@ -2,6 +2,11 @@ package dto
 
 import "time"
 
+type FederationInitiateRequestData struct {
+	FederationEndpoint string `json:"federationEndpoint" binding:"required"`
+	AuthEndpoint       string `json:"authEndpoint" binding:"required"`
+}
+
 type FederationRenewalResponseData struct {
 	FederationContextId   string    `json:"federationContextId"`
 	FederationExpiryDate  time.Time `json:"federationExpiryDate"`
--- internal/models/dto/federation_initiate_request_data.go
+++
@@ -1,6 +0,0 @@
-package dto
-
-type FederationInitiateRequestData struct {
-	FederationEndpoint string `json:"federationEndpoint" binding:"required"`
-	AuthEndpoint       string `json:"authEndpoint" binding:"required"`
-}
